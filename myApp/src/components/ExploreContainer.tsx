04a04b1e92fc09ce2b0bf32a43866c4afd1c8072
renrenpedrajeta <renrenpedrajets@gmail.com>
2026-03-04 15:12:56 +0800

    testing

--- myApp/src/components/ExploreContainer.tsx
+++ myApp/src/components/ExploreContainer.tsx
@@ -1,13 +1,18 @@
 import './ExploreContainer.css';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+        <IonToolbar>
+          <IonTitle>This is a test title</IonTitle>
+        </IonToolbar>
+    // <div id="container">
+    //   <strong>Ready to create an app?</strong>
+    //   <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    // </div>
   );
 };
 
